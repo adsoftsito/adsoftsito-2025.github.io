94fb242a6dd7349481818e22853dbfe28fbdc130
adsoftsito <adsoft@live.com.mx>
2025-03-12 15:32:45 -0600

    add services #3

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+   production:  false,
+   firebase: {
+     apiKey: "AIzaSyCCa2tCQx7lFOWBg-102L0i_G8KIP2VFYo",
+     authDomain: "my-cv-adsoft.firebaseapp.com",
+     projectId: "my-cv-adsoft",
+     storageBucket: "my-cv-adsoft.firebasestorage.app",
+     messagingSenderId: "556892922293",
+     appId: "1:556892922293:web:eee57425479eca4c1a79fc"
+   }
+};
